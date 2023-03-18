6fd578bd66823264d23f27f5bda86d3cf47a0e62
Roland Sebestyen <roland@Rolands-MacBook-Air.local>
2023-03-18 15:05:30 +0200

    Added shortcuts to open and close tree explorer

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,1 +1,6 @@
-require("nvim-tree").setup()
+local tree = require("nvim-tree");
+
+tree.setup();
+
+vim.keymap.set("n", "<leader>ot", vim.cmd.NvimTreeFocus, { desc = "[O]pen [T]ree Explorer" })
+vim.keymap.set("n", "<leader>ct", vim.cmd.NvimTreeClose, { desc = "[C]close [T]ree Explorer" })
