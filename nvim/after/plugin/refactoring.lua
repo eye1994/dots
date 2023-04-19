3b2c3ba747b9052f70e5e4b2f1edd51b58674263
Roland Sebestyen <sebestyenroland21@gmail.com>
2023-04-19 08:21:13 +0300

    Update

--- nvim/after/plugin/refactoring.lua
+++ nvim/after/plugin/refactoring.lua
@@ -1,27 +1,6 @@
-require('refactoring').setup({
-    prompt_func_return_type = {
-        go = false,
-        java = false,
+require('refactoring').setup({})
 
-        cpp = false,
-        c = false,
-        h = false,
-        hpp = false,
-        cxx = false,
-    },
-    prompt_func_param_type = {
-        go = false,
-        java = false,
-
-        cpp = false,
-        c = false,
-        h = false,
-        hpp = false,
-        cxx = false,
-    },
-    printf_statements = {},
-    print_var_statements = {},
-})
+require("telescope").load_extension("refactoring")
 
 -- remap to open the Telescope refactoring menu in visual mode
 vim.api.nvim_set_keymap(
